b6dc3b1fccf05e06bfbbce1c8ae9cf4acc7163fd
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-01-16 05:58:28 -0500

    added applicationUser model and proudct model

---
+++ BangazonSite/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BangazonSite.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BangazonSite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BangazonSite/Models/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonSite.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [Display(Name = "Date Created")]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public string City { get; set; }
+
+        [Display(Name = "Image")]
+        public byte[] ProductImage { get; set; }
+
+        [Display(Name = "Local Delivery")]
+        public bool LocalDelivery { get; set; }
+
+
+        
+
+        
+       
+        // Need to add: relationships between ProductType and Product and relationships between OrderProducts and Products
+
+
+    }
+}
